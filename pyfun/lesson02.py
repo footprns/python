7b34d051e88f7b71c26c1b96520d19adec034a63
Iman Kurniawan <ImanK@slb.com>
2018-07-07 23:42:59 +0530

    add new file

---
+++ pyfun/lesson02.py
@@ -0,0 +1,11 @@
+def square(x):
+    return x * # XXX:
+
+def launch_missiles():
+    print("Missiles launched")
+
+def even_or_odd(n):
+    if n % 2 == 0:
+        print ("even")
+        return
+    print("odd")
